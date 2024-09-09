fbf981913ba546cfa96add99b42cd25bb847d5b7
Jacob Young <jgyoung618@gmail.com>
2024-09-09 02:17:07 -0400

    GoalForm, GoalItem, updated server, updated Dashboard

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const goalRoutes = require("./routes/goalRoutes");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,9 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", goalRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
